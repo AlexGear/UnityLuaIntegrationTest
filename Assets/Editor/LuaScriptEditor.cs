0e519a00c084f3ca742895fd05fa503809d6dadd
Alex <alexey-bycov@yandex.ru>
2019-01-09 17:30:27 +0700

    Implement lua script runner

---
+++ Assets/Editor/LuaScriptEditor.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(LuaScript))]
+public class LuaScriptEditor : Editor
+{
+    public override void OnInspectorGUI() {
+        DrawDefaultInspector();
+        LuaScript script = (LuaScript)target;
+
+        if(!Application.isPlaying)
+            return;
+        
+        if(string.IsNullOrEmpty(script.error)) {
+            EditorGUILayout.HelpBox("OK", MessageType.Info);
+        }
+        else {
+            EditorGUILayout.HelpBox(script.error, MessageType.Error);
+        }
+    }
+}
---
+++ Assets/LuaScript.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NLua;
+
+public class LuaScript : MonoBehaviour
+{
+    [SerializeField, TextArea(20, 40)] string code = "";
+
+    private string oldCode;
+    public string error { get; private set; }
+
+    private Lua lua;
+
+    private void Awake() {
+        oldCode = code;
+        lua = new Lua();
+        lua.LoadCLRPackage();
+        lua["transform"] = transform;
+        DoString();
+    }
+
+    private void DoString() {
+        try {
+            lua.DoString(code);
+            error = null;
+        }
+        catch(NLua.Exceptions.LuaException e) {
+            error = FormatException(e);
+        }
+    }
+
+    void Update() {
+        if(oldCode != code) {
+            oldCode = code;
+            DoString();
+        }
+        CallLuaUpdate();
+    }
+
+    private void CallLuaUpdate() {
+        LuaFunction func = lua.GetFunction("Update");
+        if(func == null)
+            return;
+        try {
+            func.Call();
+        }
+        catch(NLua.Exceptions.LuaException e) {
+            Debug.LogError(FormatException(e), gameObject);
+            throw e;
+        }
+    }
+
+    public static string FormatException(NLua.Exceptions.LuaException e) {
+        string source = (string.IsNullOrEmpty(e.Source)) ? "<no source>" : e.Source.Substring(0, e.Source.Length - 2);
+        return string.Format("{0}\nLua (at {2})", e.Message, string.Empty, source);
+    }
+}
